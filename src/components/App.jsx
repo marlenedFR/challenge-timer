e44edfd41a4ec9392012a9c0651edaf022d32825
Marlène Dubois <dubois.marlene.pro@gmail.com>
2024-06-27 14:09:54 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,35 @@
-import { useState } from "react";
-import TimerInput from "./TimerInput";
-import Timer from "./Timer";
-import Footer from "./Footer";
+import React, { useState } from "react";
+// import TimerInput from "./TimerInput";
+// import Timer from "./Timer";
+// import Footer from "./Footer";
 import "../index.css";
-import DebugLog from "./DebugLog";
+// import DebugLog from "./DebugLog";
 
 const App = () => {
-  const [timers, setTimers] = useState([]);
+  // const [timers, setTimers] = useState([]);
 
-  const addTimer = (time) => {
-    setTimers((prevTimers) => [...prevTimers, time]);
-  };
+  const [isLoaded, setIsLoaded] = useState(false);
 
-  const removeTimer = (index) => {
-    setTimers((prevTimers) => prevTimers.filter((_, i) => i !== index));
-  };
+  React.useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  if (!isLoaded) {
+    return <div>Chargement de lapplication...</div>;
+  }
+
+  // const addTimer = (time) => {
+  //   setTimers((prevTimers) => [...prevTimers, time]);
+  // };
+
+  // const removeTimer = (index) => {
+  //   setTimers((prevTimers) => prevTimers.filter((_, i) => i !== index));
+  // };
 
   return (
     <div className="app">
-      <h1>Timer</h1>
-      <TimerInput onAddTimer={addTimer} />
-      <div className="timer-list">
-        {timers.map((time, index) => (
-          <Timer
-            key={index}
-            initialTime={time}
-            onRemove={() => removeTimer(index)}
-          />
-        ))}
-        {/* Timer de test statique */}
-        <Timer
-          initialTime={{ hours: 0, minutes: 1, seconds: 0 }}
-          onRemove={() => {}}
-        />
-        <DebugLog />
-      </div>
-      <Footer />
+      <h1>Timer App</h1>
+      <p>Si vous voyez ceci, lapplication se charge correctement.</p>
     </div>
   );
 };
